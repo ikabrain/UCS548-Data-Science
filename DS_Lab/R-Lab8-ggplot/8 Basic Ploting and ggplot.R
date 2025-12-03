c7f5bcfd9e38d2c9f3487705863e152473819857
ikansh <46121461+Ikansh@users.noreply.github.com>
2025-12-03 22:11:37 +0530

    study ggplot2 theory

--- DS_Lab/R-Lab8-ggplot/8 Basic Ploting and ggplot.R
+++ DS_Lab/R-Lab8-ggplot/8 Basic Ploting and ggplot.R
@@ -14,42 +14,41 @@ y
 # 1.1 Plot x
 plot(x)
 plot(x, main = "X data")
-plot(x, main = "X data",xlab = "X Axis", ylab="Y Axis")
+plot(x, main = "X data", xlab = "X Axis", ylab = "Y Axis")
 
 
 # 1.2 Scatter Plot; x vs y
-plot(x, y, main = "Scatter plot",xlab = "X Axis", ylab="Y Axis")
+plot(x, y, main = "Scatter plot", xlab = "X Axis", ylab = "Y Axis")
 
 
 # 1.3 Saving the Plot to a file
-png(filename="MyPlot.png")
-plot(x, y, main = "Scatter plot",xlab = "X Axis", ylab="Y Axis")
+file_path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+file_path
+png(filename = paste(file_path, "/MyPlot.png", sep = ""))
+plot(x, y, main = "Scatter plot", xlab = "X Axis", ylab = "Y Axis")
 dev.off()
-getwd()   # Get the Current Working Directory, Similar to pwd in linux
-
-
+getwd() # Get the Current Working Directory, Similar to pwd in linux
 
 #-------------------------------------------------------------
 # Step 2: The Normal Distribution
 #-------------------------------------------------------------
-x1 <- rnorm(10,sd=2)
+x1 <- rnorm(10, sd = 2)
 x1
-x2 <- rnorm(10,sd=3)
+x2 <- rnorm(10, sd = 3)
 x2
-x3 <- rnorm(10,sd=4)
+x3 <- rnorm(10, sd = 4)
 x3
 
 # Plot x1,x2,x3 together
-df <- data.frame(x1,x2,x3)
-plot(df[,1:3])
-
+df <- data.frame(x1, x2, x3)
+plot(df[, 1:3])
 
 
 #-------------------------------------------------------------
 # Step 3: Min, Max, Sum, Std Deviation
 #         Mean, Median, Mode
 #-------------------------------------------------------------
-x <- c(2,7,3,2,4,1,8,2,4,2,8,-5,4)
+x <- c(2, 7, 3, 2, 4, 1, 8, 2, 4, 2, 8, -5, 4)
 min(x)
 max(x)
 sum(x)
@@ -60,8 +59,8 @@ median(x)
 
 # To get mode (Most occured element in a list)
 getmode <- function(v) {
-  uniqv <- unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
+    uniqv <- unique(v)
+    uniqv[which.max(tabulate(match(v, uniqv)))]
 }
 
 getmode(x)
@@ -71,9 +70,9 @@ getmode(x)
 # Step 4: Correlation, Variance and Covariance (Matrices)
 #-------------------------------------------------------------
 
-x <- c(2,7,3,2,4,1,8,2,4,2,8,-5,4)
-y <- c(6,3,8,1,3,6,8,2,3,5,8,4,-6)
-z <- round(runif(13,min=-10, max=10),0)
+x <- c(2, 7, 3, 2, 4, 1, 8, 2, 4, 2, 8, -5, 4)
+y <- c(6, 3, 8, 1, 3, 6, 8, 2, 3, 5, 8, 4, -6)
+z <- round(runif(13, min = -10, max = 10), 0)
 
 # Variance
 var(x, y)
@@ -90,8 +89,8 @@ cor(x, y, method = "spearman")
 
 
 # Pairwise correlation
-df <- data.frame(x,y,z)
-pairCor <- cor(df,use="pairwise",method="pearson")
+df <- data.frame(x, y, z)
+pairCor <- cor(df, use = "pairwise", method = "pearson")
 pairCor
 
 
@@ -110,9 +109,9 @@ y <- c(80, 68, 72, 75, 70, 65, 62, 60, 85, 92)
 
 linearModel <- lm(y ~ x)
 plot(x, y)
-abline(linearModel, lwd=2)
+abline(linearModel, lwd = 2)
 predicted <- predict(linearModel)
-segments(x, y, x, predicted, col="red")
+segments(x, y, x, predicted, col = "red")
 
 
 #-------------------------------------------------------------
@@ -122,9 +121,10 @@ segments(x, y, x, predicted, col="red")
 install.packages("ggplot2")
 library(ggplot2)
 
-data(diamonds)  # Loading build-in dataset
+data(diamonds) # Loading build-in dataset
 head(diamonds)
-summary(diamonds) #Summarize the dataset
+summary(diamonds) # Summarize the dataset
+
 
 # 6.1 Histogram
 hist(diamonds$carat, main = "Carat Histogram", xlab = "Carat")
@@ -152,15 +152,18 @@ boxplot(diamonds$carat)
 
 
 boxplot(diamonds)
-boxplot(diamonds[,-c(7)]) # dropping carat column
+boxplot(diamonds[, -c(7)]) # dropping price column
 
 
 # 6.3 Histograms and Densities
-ggplot(data = diamonds) + geom_histogram(aes(x = carat))
-ggplot(data = diamonds) + geom_density(aes(x = carat), fill = "grey50")
+ggplot(data = diamonds) +
+    geom_histogram(aes(x = carat))
+ggplot(data = diamonds) +
+    geom_density(aes(x = carat), fill = "grey50")
 
 # Scatter Plot
-ggplot(diamonds, aes(x = carat, y = price)) + geom_point()
+ggplot(diamonds, aes(x = carat, y = price)) +
+    geom_point()
 
 g <- ggplot(diamonds, aes(x = carat, y = price))
 g + geom_point(aes(color = color))
@@ -168,15 +171,27 @@ g + geom_point(aes(color = color)) + facet_wrap(~color)
 g + geom_point(aes(color = color)) + facet_grid(cut ~ clarity)
 
 
-ggplot(diamonds, aes(x = carat)) + geom_histogram() + facet_wrap(~color)
-ggplot(diamonds, aes(y = carat, x = 1)) + geom_boxplot()
-ggplot(diamonds, aes(y = carat, x = cut)) + geom_boxplot()
-ggplot(diamonds, aes(y = carat, x = cut)) + geom_violin()
+ggplot(diamonds, aes(x = carat)) +
+    geom_histogram() +
+    facet_wrap(~color)
+ggplot(diamonds, aes(y = carat, x = 1)) +
+    geom_boxplot()
+ggplot(diamonds, aes(y = carat, x = cut)) +
+    geom_boxplot()
+ggplot(diamonds, aes(y = carat, x = cut)) +
+    geom_violin()
 
-ggplot(diamonds, aes(y = carat, x = cut)) + geom_point() + geom_violin()
-ggplot(diamonds, aes(y = carat, x = cut)) + geom_violin() + geom_point()
 
-ggplot(economics, aes(x = date, y = pop)) + geom_line()
+ggplot(diamonds, aes(y = carat, x = cut)) +
+    geom_point() +
+    geom_violin()
+ggplot(diamonds, aes(y = carat, x = cut)) +
+    geom_violin() +
+    geom_point()
+
+data(economics)
+ggplot(economics, aes(x = date, y = pop)) +
+    geom_line()
 
 
 # 6.3 More Plots using ggplots
@@ -186,35 +201,38 @@ install.packages("scales")
 
 library(lubridate)
 library(scales)
+
+head(economics)
 economics$year <- year(economics$date)
-economics$month <- month(economics$date, label=TRUE)
+economics$month <- month(economics$date, label = TRUE)
 econ2000 <- economics[which(economics$year >= 2000), ]
 
 # build the foundation of the plot
-g <- ggplot(econ2000, aes(x=month, y=pop))
+g <- ggplot(econ2000, aes(x = month, y = pop))
 
 # add lines color coded and grouped by year
 # the group aesthetic breaks the data into separate groups
-g <- g + geom_line(aes(color=factor(year), group=year))
+g <- g + geom_line(aes(color = factor(year), group = year))
 
 # name the legend "Year"
-g <- g + scale_color_discrete(name="Year")
+g <- g + scale_color_discrete(name = "Year")
 
 # format the y axis
-g <- g + scale_y_continuous(labels=comma)
+g <- g + scale_y_continuous(labels = comma)
 
 # add a title and axis labels
-g <- g + labs(title="Population Growth", x="Month", y="Population")
+g <- g + labs(title = "Population Growth", x = "Month", y = "Population")
 
 # plot the graph
 g
 
 
-# 6.4 Mpore on ggplots
+# 6.4 More on ggplots
 install.packages("ggthemes")
 library(ggthemes)
 # build a plot and store it in g2
-g2 <- ggplot(diamonds, aes(x=carat, y=price)) + geom_point(aes(color=color))
+g2 <- ggplot(diamonds, aes(x = carat, y = price)) +
+    geom_point(aes(color = color))
 # apply a few themes
 g2 + theme_economist() + scale_colour_economist()
 g2 + theme_excel() + scale_colour_excel()
@@ -222,14 +240,13 @@ g2 + theme_tufte()
 g2 + theme_wsj()
 
 
-
 #-------------------------------------------------------------
 # Step 7: Grid and a curve plotting
 #-------------------------------------------------------------
 
-x <- round(runif(13,min=-5, max=5),0)
-y <- round(runif(13,min=-5, max=5),0)
-z <- round(runif(13,min=-5, max=5),0)
+x <- round(runif(13, min = -5, max = 5), 0)
+y <- round(runif(13, min = -5, max = 5), 0)
+z <- round(runif(13, min = -5, max = 5), 0)
 
 plot(z, type = "n")
 grid(lty = 1, lwd = 2)
@@ -239,11 +256,11 @@ points(z^2, pch = 20)
 
 
 #-------------------------------------------------------------
-# Step 8: Grphical Parameters
+# Step 8: Graphical Parameters
 #-------------------------------------------------------------
 
-plot(z, type = "o", col = 'red', pch = 16, cex = 2)
-plot(z, col = c('red', 'blue'), pch = "+", cex = 2)
+plot(z, type = "o", col = "red", pch = 16, cex = 2)
+plot(z, col = c("red", "blue"), pch = "+", cex = 2)
 
 
 #-------------------------------------------------------------
@@ -265,7 +282,7 @@ a
 levels(a) <- LETTERS[1:5]
 a
 plot(a)
-plot(y ~ a)   # Box Plot
+plot(y ~ a) # Box Plot
 
 
 #-------------------------------------------------------------
@@ -273,7 +290,7 @@ plot(y ~ a)   # Box Plot
 #-------------------------------------------------------------
 
 # scatterplot matrix: a matrix of all pairwise bivariate scatterplots.
-iris        # Dataset
+iris # Dataset
 head(iris)
 class(iris)
 plot(iris)
@@ -289,5 +306,3 @@ plot(cos, from = -2 * pi, to = 2 * pi)
 damped.sin <- function(x) sin(5 * x) * exp(-x^2) ## New function
 class(damped.sin)
 plot(damped.sin, from = -pi, to = pi)
-
-
